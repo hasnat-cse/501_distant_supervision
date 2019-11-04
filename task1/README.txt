cf50251d811d54650d0495d5b97635253111eec8
Arif Hasnat <hasnat@ualberta.ca>
2019-11-03 17:41:50 -0700

    entity tag removal

--- task1/README.txt
+++ task1/README.txt
@@ -4,7 +4,7 @@ Team Members:
 
 Execution Instruction:
     run the following command in command line:
-        python3 task1.py DATA_FOLDER_PATH
+        python3 task1/task1.py DATA_FOLDER_PATH
 
     example:
         python3 task1.py a4_data
--- task1/task1.py
+++ task1/task1.py
@@ -2,6 +2,26 @@ import glob
 import json
 import os
 import sys
+import re
+
+
+# replace '[[ Natural Gas | /m/05k4k ]]' with 'Natural Gas'
+def remove_entity_tags_from_sentence(sentence):
+
+    # pattern to find '[[ Natural Gas | /m/05k4k ]]'
+    entity_tag_pattern = "\[\[\s*(.+?)\s+\|.+?\]\]"
+    entities = re.findall(entity_tag_pattern, sentence)
+
+    modified_sentence = sentence
+    for entity in entities:
+
+        # pattern to find '[[ Natural Gas | /m/05k4k ]]' for entity 'Natural Gas'
+        pattern = "\[\[\s*" + entity + "\s*\|.+?\]\]"
+
+        # replace '[[ Natural Gas | /m/05k4k ]]' for entity 'Natural Gas' with 'Natural Gas'
+        modified_sentence = re.sub(pattern, entity, modified_sentence)
+
+    return modified_sentence
 
 
 def main():
@@ -20,7 +40,12 @@ def main():
             # print(json.dumps(json_data, indent=4))
 
             for each_data in json_data:
-                print(each_data['sentence'])
+                sentence = each_data['sentence']
+                # print(sentence)
+
+                # remove entity tags from each sentence
+                modified_sentence = remove_entity_tags_from_sentence(sentence)
+                print(modified_sentence)
 
 
 if __name__ == "__main__":
